6c724ca63919eaf7f929c2f5fe97b69fea9259d9
Brama20 <48474997+Brama20@users.noreply.github.com>
2019-12-28 21:56:31 -0500

    Project 4 complete

---
+++ Nand2Tetris/Projects/04/mult.asm
@@ -0,0 +1,41 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+    @0
+    D=A
+    @R2
+    M=D        //Reset product to 0
+
+    @R1
+    D=M
+    @i
+    M=D
+
+(LOOP)
+    @i
+    D=M
+    @END
+    D;JEQ      //If i==0 goto END@
+    
+    @R0
+    D=M 
+    @R2 
+    M=D+M      //R2+=R1 
+   
+    @i
+    M=M-1      //i--
+    
+    @LOOP
+    0;JMP
+
+
+(END)
+    @END
+    0;JMP
